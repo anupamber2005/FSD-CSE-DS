d36fd0b52d480bacb5d168c7d6c9b1d250daf343
anupamber2005 <anup.22b1541168@abes.ac.in>
2024-10-17 10:27:37 +0530

    first

---
+++ Day_3/Day_4/p1.js
@@ -0,0 +1,17 @@
+/ let x = '12';
+// let num = Number(x);
+// let y ='12'%'4';
+// let b = Boolean(12);
+// // console.log(b);
+// function display(){
+//     // let c = confirm("Are you sure to Logout");
+//     let c =prompt("Enter your name");
+//     alert("Welcome");
+// }
+
+// let sayhello =()=>{
+//     console.log("Arrow Function");
+
+// }
+let sayhello = () => console.log("Arrow Function");
+sayhello();
